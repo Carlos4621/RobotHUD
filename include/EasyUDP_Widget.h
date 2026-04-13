6124ff9581d0946551b1b43f28c8bbdf61d5e839
Carlos Iván <carlosivanhb@hotmail.com>
2026-04-13 09:29:51 -0600

    Documentación añadida. Atributo nodiscard para getDevice

--- include/EasyUDP_Widget.h
+++ include/EasyUDP_Widget.h
@@ -10,15 +10,22 @@ class QWidget;
 namespace Ui { class Dialog; }
 QT_END_NAMESPACE
 
+/// @brief Widget que permite la conexión de un objeto EasyUDP
 class EasyUDP_Widget : public QDialog 
 {
     Q_OBJECT
 
 public:
+
+    /// @brief Constructor base
+    /// @param parent Padre del widget
     explicit EasyUDP_Widget(QWidget* parent = nullptr);
 
     ~EasyUDP_Widget() noexcept;
 
+    /// @brief Obtiene el objeto EasyUDP, debe ser usado junto a la señal succesfulConnection para evitar punteros nulos
+    /// @return El dispositivo configurado, null en caso de no haber conexión
+    [[nodiscard]]
     std::shared_ptr<EasyUDP> getDevice();
 
 signals:
